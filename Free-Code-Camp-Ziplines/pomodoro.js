4587c8e927def27fd06a9a3c1823d1da7d2a5506
su6a12 <su6a12m3lon@gmail.com>
2015-08-10 23:46:21 -0700

    continuous intervals

--- Free-Code-Camp-Ziplines/pomodoro.js
+++ Free-Code-Camp-Ziplines/pomodoro.js
@@ -25,20 +25,24 @@ pomodoroApp.controller("pomodoroController", ["$scope", "$timeout", function($sc
 			}
 		}
 
-		// randomize color of countdown text
-		var red = Math.floor(Math.random() * 255);
+		var red = Math.floor(Math.random() * 255);					// randomize color of countdown text
 		var green = Math.floor(Math.random() * 255);
 		var blue = Math.floor(Math.random() * 255);
 		$("#timer").css("color", "rgb(" + red + ", " + green + ", " + blue + ")");
-		
+
 		$scope.countdown = "" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
 
 		if (minutes === 0 && seconds === 0) {
 			audio.play();
-			$scope.isBegin = "pause";		// break immediately begins
-			$scope.countdown = $scope.breakTime;
+			$scope.isBegin = "pause";													// break immediately begins
+			if ($scope.countdown === $scope.workTime) {				// check which countdown: work or break			
+				$scope.countdown = $scope.breakTime;
+			}
+			else {
+				$scope.countdown = $scope.workTime;
+			}
 			minutes = $scope.countdown;
-			timeout = $timeout($scope.updateTimer, 1000);		// start break countdown
+			timeout = $timeout($scope.updateTimer, 1000);			// start break countdown
 		}
 		else {
 			timeout = $timeout($scope.updateTimer, 1000);
@@ -49,33 +53,33 @@ pomodoroApp.controller("pomodoroController", ["$scope", "$timeout", function($sc
 		if (!isPlaying) {
 			timeout = $timeout($scope.updateTimer, 1000);
 			isPlaying = true;
-			$scope.isBegin = "pause";		// currently playing
+			$scope.isBegin = "pause";													// currently playing
 		}
 		else {
 			$timeout.cancel(timeout);
 			isPlaying = false;
-			$scope.isBegin = "resume";		// currently paused
+			$scope.isBegin = "resume";												// currently paused
 		}
 	};
 
-	$scope.addTime = function(time) {
-		if (time === $scope.breakTime) {
-			$scope.breakTime++;
-		}
-		else {
+	$scope.addWork = function(time) {
+		if (time > 0) {
 			$scope.workTime++;
 			$scope.countdown++;
 		}
-	}
-	$scope.subTime = function(time) {
+	};
+	$scope.subWork = function(time) {
 		if (time > 0) {
-			if (time === $scope.breakTime) {
-				$scope.breakTime--;
-			}
-			else {
-				$scope.workTime--;
-				$scope.countdown--;
-			}
+			$scope.workTime--;
+			$scope.countdown--;
 		}
+	};
+	$scope.addBreak = function(time) {
+		if (time > 0)
+			$scope.breakTime++;
+	};
+	$scope.subBreak = function(time) {
+		if (time > 0)
+			$scope.breakTime--;
 	};
 }]);
