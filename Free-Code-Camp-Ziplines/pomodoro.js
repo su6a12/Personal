952d3c1dd45c91362e6b7dc9967e0d26f53d9a8f
su6a12 <su6a12m3lon@gmail.com>
2015-08-10 23:32:03 -0700

    Add break countdown and color change, update text sizes

--- Free-Code-Camp-Ziplines/pomodoro.js
+++ Free-Code-Camp-Ziplines/pomodoro.js
@@ -2,11 +2,11 @@ var pomodoroApp = angular.module("pomodoroApp", []);
 
 pomodoroApp.controller("pomodoroController", ["$scope", "$timeout", function($scope, $timeout) {
 	$scope.breakTime = 5;
-	$scope.workTime = 0;
+	$scope.workTime = 25;
 	$scope.countdown = $scope.workTime;
 	$scope.isBegin = "begin";
 	var minutes = $scope.countdown;
-	var seconds = 5;
+	var seconds = 0;
 	var isPlaying = false;
 	var audio = new Audio("glass_ping.mp3");
 	var timeout;
@@ -24,13 +24,21 @@ pomodoroApp.controller("pomodoroController", ["$scope", "$timeout", function($sc
 				seconds--;
 			}
 		}
-		$scope.countdown = "" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
 
+		// randomize color of countdown text
+		var red = Math.floor(Math.random() * 255);
+		var green = Math.floor(Math.random() * 255);
+		var blue = Math.floor(Math.random() * 255);
+		$("#timer").css("color", "rgb(" + red + ", " + green + ", " + blue + ")");
+		
+		$scope.countdown = "" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
 
 		if (minutes === 0 && seconds === 0) {
 			audio.play();
-			$scope.isBegin = "begin";
-			$scope.countdown = $scope.workTime;
+			$scope.isBegin = "pause";		// break immediately begins
+			$scope.countdown = $scope.breakTime;
+			minutes = $scope.countdown;
+			timeout = $timeout($scope.updateTimer, 1000);		// start break countdown
 		}
 		else {
 			timeout = $timeout($scope.updateTimer, 1000);
@@ -41,12 +49,12 @@ pomodoroApp.controller("pomodoroController", ["$scope", "$timeout", function($sc
 		if (!isPlaying) {
 			timeout = $timeout($scope.updateTimer, 1000);
 			isPlaying = true;
-			$scope.isBegin = "pause";
+			$scope.isBegin = "pause";		// currently playing
 		}
 		else {
 			$timeout.cancel(timeout);
 			isPlaying = false;
-			$scope.isBegin = "resume";
+			$scope.isBegin = "resume";		// currently paused
 		}
 	};
 
