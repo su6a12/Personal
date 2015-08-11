ac5055a34e6b143b85904cf291d2fa467cb990ba
su6a12 <su6a12m3lon@gmail.com>
2015-08-10 23:02:10 -0700

    Add quote.js file

---
+++ Free-Code-Camp-Ziplines/quote.js
@@ -0,0 +1,25 @@
+var prevQuote = 0;
+var quote = "";
+// Using Quotes on Design API
+$(".new-quote").click(function() {
+
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback=", function(data) {
+    var randQuote = Math.floor(Math.random() * (30-1));
+    
+    while (prevQuote === randQuote) {
+      randQuote = Math.floor(Math.random() * (30-1));
+    }
+    quote = data[randQuote].content.slice(3, -5);
+    quote = quote.slice(0, 140);
+    //console.log(quote);
+
+    $(".quote").html(data[randQuote].content);
+    $(".quoter").html(data[randQuote].title);
+    
+    prevQuote = randQuote;
+  }); // end of .getJSON
+});   // end of get quote button click
+  
+$(".link").click(function() {
+  $(this).attr("href", "https://twitter.com/intent/tweet" + "?text=" + quote);
+});
