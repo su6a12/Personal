dfe1c55d23fa269545d73df3e1c708d8fb3178c5
su6a12 <su6a12m3lon@gmail.com>
2015-08-18 23:16:49 -0700

    update camperNews to use angularjs, implement search feature

--- Free-Code-Camp-Ziplines/camperNews.js
+++ Free-Code-Camp-Ziplines/camperNews.js
@@ -1,35 +1,14 @@
-$(document).ready(function() {
+var camperApp = angular.module("camperApp", []);
 
-	var camperAPI = "http://www.freecodecamp.com/stories/hotStories";
-	var article, articleID, image, userName, headline;
+camperApp.controller("camperController", ["$scope", "$http", function($scope, $http) {
 
-	function camperData(data) {
-		data.forEach(function(eachArticle) {
-			userName = eachArticle.author.username;
-			headline = eachArticle.headline;
-			image = eachArticle.image || "http://placekitten.com/g/195/195";
-			article = '<div class="article-tile ' + userName + '">';
-			article += '<li>';
-			article += '<img class="article-img" src="' + image + '">';
-			//console.log(eachArticle.image);
-			article += '<span class="article-author">' + userName + '</span><br/>';
-			article += '<span class="article-headline">' + headline.substring(0, 80) + '</span>';
-			article += '</li></div>';
-			$("#news-list").append(article);
-
-			$("li").click(function() {
-				window.open(eachArticle.link);
-			});						// end of click event
-			$("#search").keyup(function() {
-				var text = $(this).val().toLowerCase();
-
-				$("#news-list>li").each(function() {
-					//console.log($(this).parent());
-					$(this).parent().nodeValue.indexOf(text) >= 0 ? $(this).show() : $(this).hide();
-				});
-			});						// end of keyup event
-		});
-	}
-	$.getJSON(camperAPI, camperData);
-
-});
+  $http({method: "GET", url: "http://www.freecodecamp.com/news/hot"}).success(function(data) {
+  	$scope.query = "";
+  	$scope.image = data.image;
+    $scope.posts = data;
+    $scope.image = function(article) {
+    	return article.image || "http://placekitten.com/g/195/195";
+    };
+    $scope.result = [];
+  });   // end of $http
+}]);    // end of controller
