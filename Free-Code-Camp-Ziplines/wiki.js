a08d67a54dbd77a2bc93e2644337deafaf8272ad
su6a12 <su6a12m3lon@gmail.com>
2015-09-06 00:20:41 -0700

    Update wiki

--- Free-Code-Camp-Ziplines/wiki.js
+++ Free-Code-Camp-Ziplines/wiki.js
@@ -1,34 +1,60 @@
-$(document).ready(function() {
+var myApp = angular.module("myApp", []);
 
-	var wikiAPI = "https://en.wikipedia.org/w/api.php&callback=?", searchText;
+myApp.controller("myController", ["$scope", "$http", function($scope, $http) {
+  var searchText, wikiAPI;
+  var wikiResult = "https://en.wikipedia.org/wiki/";
+  $scope.resultsList = [];
+  //$(function() {
+  	//$("button.btn").on("click", function() {
+  $scope.searchQuery = function() {
+	  	searchText = $("#search-field").val();
+	  	//generator returns a list of pageids, those are in turn plugged into wikipedia URL to retrieve page URL
+	  	//wikiAPI = "https://en.wikipedia.org/w/api.php?action=query&list=search&rvprop=content&format=json&srlimit=10&prop=pageimages|extracts&explaintext=true&exintro=true&exsentences=1&srsearch=" + 
+	  	//searchText + "&generator=search&gsrsearch=" + searchText + "&gsrlimit=10&callback=JSON_CALLBACK";
+	  	wikiAPI = "http://en.wikipedia.org/w/api.php?action=query&generator=search&prop=extracts&exintro=true&exlimit=max&explaintext=true&" +
+	  						"exsentences=1&pilimit=max&format=json&gsrsearch=" + searchText + "&callback=JSON_CALLBACK";
 
-	$("button").on("click", function() {
-		searchText = $("#search-field").val();
-	});
+	  	$http.jsonp(wikiAPI).
+		  	then(function(data) {
+		  		console.log(data);
+		    	var results = data.data.query.pages;
+		    	console.log(results);
+		    	$.each(results, function(key, value) {
+		    		$scope.resultsList.push({ title: value.title, snippet: value.extract, link: wikiResult + value.title.replace(/\s/g, '%20') });
+		    	});
+		    	console.log($scope.resultsList);
+		  		}, function(error) {
+		    	console.log(error);
+		  	});		// end of then callback
+ 		//});				// end of button event listener
+	}
+ 		resultsList = [];
+  //});					// end of $(function() {})
+}]);					// end of controller
 
-	// $.ajax({
-	// 	url: wikiAPI,
-	// 	datatype: "jsonp",
-	// 	type: "GET",
-	// 	data: searchText,
-	// 	success: function(data) {
-	// 		console.log(data);
-	// 	}
-	// });
+/////////////////////Old version////////////////////////////////
+// $(document).ready(function() {
 
+//   var searchText, wikiAPI;
 
-		var wikiOptions = {
-			action: "query",
-			list: "search",
-			format: "json",
-			srsearch: searchText
-		};
+//   $("button").on("click", function() {
+//     searchText = $("#search-field").val();
+//     wikiAPI = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&callback=?";
 
-		function wikiResults(data) {
-			console.log(data);
-		}
-		$.getJSON(wikiAPI, wikiOptions, wikiResults);
+//   APIOptions = {
+//     srsearch: searchText
+//   };
+//   $.getJSON(wikiAPI, APIOptions, function(data) {
+//     console.log(data);
+//   });
+//   });
+// });
 
 
 
-});
+
+
+
+
+
+
